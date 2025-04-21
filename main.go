ee0f61181ade65b22e72070155671ea6fde9e256
yemiwebby <yemiwebby@gmail.com>
2025-04-21 13:40:38 +0100

    update branch

--- main.go
+++ main.go
@@ -10,5 +10,5 @@ func updateNumber(n *int) int {
 func main() {
 	number := 5
 	update := fmt.Sprintf("the update value is: %d", updateNumber(&number))
-	fmt.Println(update)
+	fmt.Println("update", update)
 }
