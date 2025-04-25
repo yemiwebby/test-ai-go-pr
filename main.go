339d56822a56dfc21a50d528a73408a7d43eeacf
yemiwebby <yemiwebby@gmail.com>
2025-04-25 11:49:33 +0100

    update value

--- main.go
+++ main.go
@@ -10,6 +10,6 @@ func updateNumber(n *int) int {
 func main() {
 	number := 5
 	update := fmt.Sprintf("the update value is: %d", updateNumber(&number))
-	number = 44
+	number = 60
 	fmt.Println(update)
 }
